277f32211f1122f448accffab38e40b113758638
Gianfranco-Guzman <gianfranco.guzman.97@gmail.com>
2024-08-26 11:14:45 -0300

    Add files via upload

---
+++ punto_4.py
@@ -0,0 +1,14 @@
+#4) Declara 2 variables numéricas (con el valor que desees), he indica cual es mayor de
+#los dos. Si son iguales indicarlo también. Ves cambiando los valores para comprobar
+#que funciona.
+num1= 7
+num2= 7
+mayor= 0
+if (num1 == num2):
+    print("Los numeros son iguales")
+elif (num1 > num2):
+    mayor= num1
+    print("El numero mayor es: ",mayor)
+else:
+    mayor= num2
+    print("El numero mayor es: ",mayor)
