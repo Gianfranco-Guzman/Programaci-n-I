6ae181570a884dbfe86315f4a13e75da43731d2a
Gianfranco Guzmán <gianfranco.guzman.97@gmail.com>
2024-08-26 17:27:41 -0300

    Corrección de contenido duplicado

--- punto_1.py
+++ punto_1.py
@@ -3,10 +3,3 @@
 #bienvenida por consola con el nombre asignado a la variable. Por ejemplo: si asigno
 #“Fernando”, me aparezca “Bienvenido Fernando”. 
 nombre= "Fernando"
-=======
-#1) Declara una variable que contenga un nombre, después muestra un mensaje de
-#bienvenida por consola con el nombre asignado a la variable. Por ejemplo: si asigno
-#“Fernando”, me aparezca “Bienvenido Fernando”. 
-nombre= "Fernando"
->>>>>>> 277f32211f1122f448accffab38e40b113758638
-print("Bienvenido", nombre)
--- punto_11.py
+++ punto_11.py
@@ -19,38 +19,3 @@ while True:
        print("Intentos fallidos:",i)
        print("El acceso ha sido bloqueado")
        break
-    
-
- 
-
-    
-
-
-=======
-#11) Escribe una aplicación con una variable que contenga una contraseña cualquiera.
-#Después se te pedirá que introduzcas la contraseña, con 3 intentos. Cuando aciertes ya
-#no pedirá más la contraseña y mostrara un mensaje diciendo “Acceso Correcto”.
-#Piensa bien en la condición de salida (3 intentos y si acierta sale, aunque le queden
-#intentos). Si no acierta en ninguno de los 3 intentos, mostrar el mensaje “El acceso se
-#ha bloqueado después de los 3 intentos”. Fin programa.       
-i= 0
-claveAzar= 1234
-#clave=int(input("Ingrese la clave de usuario: "))
-
-while True:
-    clave=int(input("Ingrese la clave de usuario: "))
-    i= i + 1
-    if clave == claveAzar:
-        print("Acceso Correcto")
-        break
-    elif i==3:
-       print("El acceso ha sido bloqueado")
-       break
-    
-
- 
-
-    
-
-
->>>>>>> 277f32211f1122f448accffab38e40b113758638
